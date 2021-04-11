e5cc414c19ccd02605de991e75f4e4f9e9111b94
sekikentaro <mad8ai7h02031987seki@gmail.com>
2021-04-11 16:28:46 +0900

    routes追加

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@
 
 Route::get('/', function () {
     return view('welcome');
+
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
 });
