149ae655d79b0f19740f988ab58e46a5400dcef7
sekikentaro <mad8ai7h02031987seki@gmail.com>
2021-05-10 20:13:06 +0900

    ページレイアウト

--- resources/views/app_addition/index.blade.php
+++ resources/views/app_addition/index.blade.php
@@ -25,6 +25,24 @@
     </div>
 </nav>
 
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+</div>
 
 <footer class="text-center bg-dark text-white">
     <p class="py-3">APPMANAGER</p>
--- resources/views/app_manage/index.blade.php
+++ resources/views/app_manage/index.blade.php
@@ -11,22 +11,98 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand text-dark" href="{{route('app_toppage.index')}}">APPMANAGER</a> 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ">
-                <a class="nav-link text-dark" href="{{route('app_toppage.index')}}">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-link text-dark" href="{{route('app_manage.index')}}">manager</a>
-                <a class="nav-link text-dark" href="{{route('app_addition.index')}}">Addition</a>
-                <a class="nav-link text-dark" href="{{route('app_info.index')}}">Contact</a>
-            </div>
+    <a class="navbar-brand text-dark" href="{{route('app_toppage.index')}}">APPMANAGER</a> 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ">
+            <a class="nav-link text-dark" href="{{route('app_toppage.index')}}">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link text-dark" href="{{route('app_manage.index')}}">manager</a>
+            <a class="nav-link text-dark" href="{{route('app_addition.index')}}">Addition</a>
+            <a class="nav-link text-dark" href="{{route('app_info.index')}}">Contact</a>
         </div>
-    </nav>
+    </div>
+</nav>
 
-    <footer class="text-center bg-dark text-white">
-        <p class="py-3">APPMANAGER</p>
-    </footer>
+<table class="table table-bordered">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Tool name</th>
+      <th scope="col">bottn</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Slack</td>
+      <td style="text-align:right;">
+      <button>削除ボタン</button>
+      <!-- 削除ボタンを押すと確認がでる -->
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Gmail</td>
+      <td style="text-align:right;">
+      <button>削除ボタン</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Twitter</td>
+      <td style="text-align:right;">
+      <button>削除ボタン</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>LINE</td>
+      <td style="text-align:right;">
+      <button>削除ボタン</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Facebook</td>
+      <td style="text-align:right;">
+      <button>削除ボタン</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>----</td>
+      <td style="text-align:right;">
+      <button>削除ボタン</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>....</td>
+      <td style="text-align:right;">
+      <button>削除ボタン</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>@@@@@</td>
+      <td style="text-align:right;">
+      <button>削除ボタン</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>//////</td>
+      <td style="text-align:right;">
+      <button>削除ボタン</button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<footer class="text-center bg-dark text-white">
+    <p class="py-3">APPMANAGER</p>
+</footer>
 </body>
 </html>
