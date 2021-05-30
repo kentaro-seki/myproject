eb906a3cdc57655c3e73b04aecd4d6d8a329af6f
sekikentaro <mad8ai7h02031987seki@gmail.com>
2021-05-30 18:04:22 +0900

    レイアウト変更

--- resources/views/admin/contact/index.blade.php
+++ resources/views/admin/contact/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'chart')
+@section('title', 'contact')
 
 @section('content')
 <div class="container">
--- resources/views/app_addition/index.blade.php
+++ resources/views/app_addition/index.blade.php
@@ -2,9 +2,9 @@
 @section('title', 'addition')
 
 @section('content')
+
     <div class="row">
         <div class="col-md-max">
-            <h2>APPAddition</h2>
             {{-- <form action="https://www.google.com/search">
                 <div class="text-right">
                     <input id="search-input" placeholder="1つキーワードを入力" type="text" name="q" target="_blank">
@@ -44,7 +44,7 @@
             </form>
         </div>
         <div>
-            <div class="row">
+            <div class="containar">
                 <table class="table">
                     <thead>
                         <tr>
@@ -68,7 +68,6 @@
                                     </form>
                                 </td>
                                 <td>
-
                                     <form action="{{ route('app_addition.update', $app->id) }}" method="post">
                                         {{ csrf_field() }}
                                         <input type="text" value="{{ $app->url }}" name="url">
@@ -87,4 +86,7 @@
     <footer class="text-center bg-dark text-white">
         <p class="py-3">APPMANAGER</p>
     </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 @endsection
